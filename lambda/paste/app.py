a4a11542090ece54b7ad96ce0a879cbab24530e2
trumanpurnell <truman.purnell@gmail.com>
2022-06-20 01:20:36 -0600

    enables lambda to upload to s3

--- lambda/paste/app.py
+++ lambda/paste/app.py
@@ -1,7 +1,19 @@
-import sys
+import boto3
+import json
+import uuid
+import io
+
+s3 = boto3.client('s3')
+sts = boto3.client('sts')
 
 
 def handler(event, context):
-    print(event, context)
+    print(sts.get_caller_identity())
 
-    return 'Hello from AWS Lambda using Python' + sys.version + '!'
+    fileobj = io.BytesIO(event['body'].encode())
+    bucket = 'pastewin-pastes'
+    key = "%s.json" % uuid.uuid4()
+
+    s3.upload_fileobj(fileobj, bucket, key)
+
+    return key
