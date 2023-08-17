f5240b81f4e3dfa0d62e9c226a2a70e85e098ddb
trumanpurnell <truman.purnell@gmail.com>
2023-08-17 00:46:14 -0600

    fill out logic to allow users to upload their own bucket to s3, and post files

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'boto3',
+        'awyes'
     ],
     entry_points={'console_scripts': ['pastewin=pastewin.pastewin:main']},
     classifiers=[
